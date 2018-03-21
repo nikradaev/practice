39b7ac3abef332007f01a7314357d48def237a77
Nik Radaev <nik.radaev@gmail.com>
2018-03-21 13:25:15 -0600

    Update bubble-sort.py
    Optimize, shorten, beautiful

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,47 +1,23 @@
 #Buble sort array algorithm function
 
-x = [20, 5, 6, 7, 9, 10, 100, 30, 4, 0, 999, -5]
+x = [8, 9, 7, 10, 6, 4, 0, 100, -5, 30, 7, 10, 999, 200, 15]
 
-def sort (a):
-    l = len(a)
-    for j in range(l):
-        #slicing to the center
-        for i in range(l - j):
-            #find the min & swap in the original array
-            #but finding the min index only within cutoff portion [i:l-j]
-            min_index = minn(a[i : l - j])
-            #swapping with i and min_index+i - because desired location for min is i and current min is minindex + i
-            a = swap(i, min_index + i, a)
-            #find the max & swap in the original array
-            #but finding the max index only within cutoff portion [i:l-j]
-            max_index = maxx(a[i : l - j])
-            #swapping with l - j - 1 and max_index+i - because desired location for max is l - j - 1 and current max is maxindex + i
-            a = swap(l - j - 1, max_index + i, a)
+def bubbleSort(a):
+    def swap(x, y):
+        temp = a[x]
+        a[x] = a[y]
+        a[y] = temp
+    #outer loop
+    for j in range(len(a)): 
+        #slicing to the center, inner loop, python style
+        for i in range(j, len(a) - j):  
+            #find the min index and swap
+            if a[i] < a[j]:
+                swap(j, i)
+            #find the max index and swap
+            if a[i] > a[len(a) - j - 1]:
+                swap(len(a) - j - 1, i)
     return a
-    
-def minn(a):
-    min_value = a[0]
-    min_index = 0
-    for x in range(len(a)):
-        if a[x] < min_value:
-            min_value = a[x]
-            min_index = x
-    return min_index
 
-def maxx(a):
-    max_value = a[0]
-    max_index = 0
-    for x in range(len(a)):
-        if a[x] > max_value:
-            max_value = a[x]
-            max_index = x
-    return max_index
-
-def swap(index1, index2, arr):
-    temp = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = temp
-    return arr
-    
-x = sort(x)
+x = bubbleSort(x)
 print(x)
